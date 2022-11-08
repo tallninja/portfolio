ba81daa8c1b449144f2e78ebd175b3666a7dab35
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-08 07:15:00 +0300

    Removed unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Blogs, PageNotFound, Projects } from './pages';
 import { Home } from './pages';
 
