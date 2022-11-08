c46ee4db18d3ade0469812d6a8acb794dcd35af0
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-08 19:08:56 +0300

    Added featured projects

--- src/components/featured-projects/featured-projects.tsx
+++ src/components/featured-projects/featured-projects.tsx
@@ -3,9 +3,14 @@ import { Card } from '../card';
 export const FeaturedProjects = () => {
 	return (
 		<div className='mt-8'>
-			<h2 className='my-4 font-extrabold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-8 md:text-4xl md:leading-10'>
-				Featured Projects 🏗
-			</h2>
+			<div className='flex align-baseline justify-between my-4'>
+				<h2 className='font-extrabold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-8 md:text-4xl md:leading-10'>
+					Featured Projects 🏗
+				</h2>
+				<a className='text-base font-medium leading-6 text-yellow-500 hover:text-yellow-600 dark:hover:text-yellow-400'>
+					All projects →
+				</a>
+			</div>
 			<hr />
 			<div className='container py-6'>
 				<div className='flex flex-wrap -m-4'>
