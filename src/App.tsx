94c0ae968fe390a71a23be6b8342834a9d17ec2b
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-08 07:35:18 +0300

    Adjusted the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components';
 import { Blogs, Home, PageNotFound, Projects } from './pages';
 
 export const App = () => {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/blogs' element={<Blogs />} />
-				<Route path='/projects' element={<Projects />} />
-				<Route path='*' element={<PageNotFound />} />
-			</Routes>
+			<div className='antialiased bg-white text-black dark:bg-black dark:text-white h-screen'>
+				<Navbar />
+				<Routes>
+					<Route path='/'>
+						<Route index element={<Home />} />
+						<Route path='blogs' element={<Blogs />} />
+						<Route path='projects' element={<Projects />} />
+					</Route>
+					<Route path='*' element={<PageNotFound />} />
+				</Routes>
+			</div>
 		</BrowserRouter>
 	);
 };
